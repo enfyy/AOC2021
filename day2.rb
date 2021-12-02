dc754b5f6ee2d72883282d009bff9b8efe739179
jbannert <enfy@rocketmail.com>
2021-12-02 20:59:08 +0100

    day2

---
+++ day2.rb
@@ -0,0 +1,34 @@
+@input = File.open("inputs/day2_input.txt").read.split("\n").map { |cmd| cmd.strip }
+
+def part1()
+  x = 0
+  y = 0
+  @input.each do |command|
+    steps = command.split(' ')[1].to_i
+    x += steps if command.start_with?('forward')
+    y += steps if command.start_with?('down')
+    y -= steps if command.start_with?('up')
+    [x, y]
+  end
+  x * y
+end
+
+def part2()
+  x = 0
+  y = 0
+  aim = 0
+  @input.each do |command|
+    steps = command.split(' ')[1].to_i
+    if command.start_with?('forward')
+      x += steps
+      y += aim * steps
+    end
+    aim += steps if command.start_with?('down')
+    aim -= steps if command.start_with?('up')
+    [x, y]
+  end
+  x * y
+end
+
+pp part1()
+pp part2()
