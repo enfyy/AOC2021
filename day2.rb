5a540e7ebc65155963404b0978bd782a1dffe852
jbannert <enfy@rocketmail.com>
2021-12-02 21:04:47 +0100

    day2

--- day2.rb
+++ day2.rb
@@ -1,22 +1,18 @@
 @input = File.open("inputs/day2_input.txt").read.split("\n").map { |cmd| cmd.strip }
 
 def part1()
-  x = 0
-  y = 0
+  x = y = 0
   @input.each do |command|
     steps = command.split(' ')[1].to_i
     x += steps if command.start_with?('forward')
     y += steps if command.start_with?('down')
     y -= steps if command.start_with?('up')
-    [x, y]
   end
   x * y
 end
 
 def part2()
-  x = 0
-  y = 0
-  aim = 0
+  x = y = aim = 0
   @input.each do |command|
     steps = command.split(' ')[1].to_i
     if command.start_with?('forward')
@@ -25,7 +21,6 @@ def part2()
     end
     aim += steps if command.start_with?('down')
     aim -= steps if command.start_with?('up')
-    [x, y]
   end
   x * y
 end
