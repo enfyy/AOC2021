bc43bf91645a45b213c0a2cb2e69b3e334fe7166
jbannert <enfy@rocketmail.com>
2021-12-03 18:58:53 +0100

    day3

---
+++ day3.rb
@@ -0,0 +1,23 @@
+@input = File.open("inputs/day3_input.txt").read.split("\n").map { |cmd| cmd.strip }
+
+def part1
+  gamma_string = ''
+  @input.map{ |row| row.split('').map{ |num| num.to_i}}.transpose.each{ |col| col.count(0) < col.count(1) ? gamma_string += '1' : gamma_string += '0' }
+  gamma_string.to_i(2) * gamma_string.gsub('0', 'x').gsub('1', '0').gsub('x', '1').to_i(2) #lmao
+end
+
+def get_rating(keep_majority)
+  rows = @input.map{ |row| row.split('').map{ |num| num.to_i}}
+  i = 0
+  until rows.count == 1
+    cols = rows.transpose
+    cols[i].count(0) <= cols[i].count(1) ? rows.delete_if { |row| row[i] == (!keep_majority && 1 || 0) } : rows.delete_if { |row| row[i] == (keep_majority && 1 || 0) }
+    i += 1
+  end
+  rows.first.join.to_i(2)
+end
+
+# part1
+pp part1
+# part 2
+pp get_rating(true) * get_rating(false) #part2
